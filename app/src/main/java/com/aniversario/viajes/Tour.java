b1e18694f01bbefc5ffa65823f71201bb0093ac9
RosalesJhordi <1428060@senati.pe>
2024-10-05 17:10:48 -0500

    first commit

---
+++ app/src/main/java/com/aniversario/viajes/Tour.java
@@ -0,0 +1,39 @@
+package com.aniversario.viajes;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+public class Tour extends Fragment {
+
+    private WebView webView;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_tour, container, false);
+        webView = view.findViewById(R.id.webview4);
+        webView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("https://www.incasperu.com/viajes-peru/peru-al-completo/");
+        return view;
+    }
+
+    public boolean canGoBack() {
+        return webView != null && webView.canGoBack();
+    }
+
+    public void goBack() {
+        if (webView != null) {
+            webView.goBack();
+        }
+    }
+}
